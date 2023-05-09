ecfa2795bf98900cc18d5b5ba9d3dad57b234fe6
tispBe <timo.spring@zuehlke.com>
2023-05-09 15:40:31 +0200

    Use mocked chatgpt response

--- frontend/src/components/attraction-information/attraction-information.tsx
+++ frontend/src/components/attraction-information/attraction-information.tsx
@@ -1,6 +1,7 @@
 import { component$, useSignal, useTask$ } from '@builder.io/qwik';
 import { Configuration, OpenAIApi } from 'openai';
 import styles from './attraction-information.module.css';
+import mockFact from '../../mocks/chat-gpt-generation.json';
 
 const configuration = new Configuration({
   apiKey: process.env.CHATGPT_API_KEY,
@@ -11,16 +12,22 @@ export default component$(() => {
   const attractionFact = useSignal<string>('');
 
   useTask$(async () => {
-    const response = await openai.createCompletion({
-      model: 'text-davinci-003',
-      prompt:
-        'Tell me something about Bern in Switzerland in less than 50 words',
-      temperature: 0.8,
-      max_tokens: 50,
-    });
-    attractionFact.value =
-      response.data.choices[0].text || 'Could not fetch information';
+    // const response = await openai.createCompletion({
+    //   model: 'text-davinci-003',
+    //   prompt:
+    //     'Tell me something about Bern in Switzerland in less than "50" words and complete sentences.',
+    //   temperature: 0.8,
+    //   max_tokens: 50,
+    // });
+    // attractionFact.value =
+    //   response.data.choices[0].text || 'Could not fetch information';
+    const response = await mockFact;
+    attractionFact.value = response.choices[0].text;
   });
 
-  return <div class={styles['fact']}>{attractionFact.value}</div>;
+  return (
+    <div>
+      <div class={styles['fact']}>{attractionFact.value}</div>;
+    </div>
+  );
 });
