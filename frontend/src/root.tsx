88cc095febec4360d5c2abdcb21f6e3a92434f8a
Schlumpf, Stefanie <stefanie.schlumpf2@zuehlke.com>
2023-05-09 17:06:38 +0200

    add title to root.tsx

--- frontend/src/root.tsx
+++ frontend/src/root.tsx
@@ -20,7 +20,8 @@ export default component$(() => {
         <link rel="preconnect" href="https://fonts.googleapis.com"></link>
         <link rel="preconnect" href="https://fonts.gstatic.com"></link>
         <link href="https://fonts.googleapis.com/css2?family=Patrick+Hand&display=swap" rel="stylesheet"></link>
-          <RouterHead />
+        <RouterHead />
+        <title>roam a llama</title>
       </head>
       <body lang="en">
         <RouterOutlet />
