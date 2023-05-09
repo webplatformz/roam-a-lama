c0502699852f5c490fff59fc6cb8f3c614ea2715
Julio Cesar Castro Lopez <caju@surfer-172-29-4-37-hotspot.internet-for-guests.com>
2023-05-09 17:09:18 +0200

    Restrict files that get sent to Deno Deploy

--- frontend/src/routes/layout.tsx
+++ frontend/src/routes/layout.tsx
@@ -1,11 +1,11 @@
-import { component$, useStyles$ } from '@builder.io/qwik';
-import { routeLoader$ } from '@builder.io/qwik-city';
+import { component$, Slot, useStyles$ } from "@builder.io/qwik";
+import { routeLoader$ } from "@builder.io/qwik-city";
 
-import Header from '~/components/starter/header/header';
-import Footer from '~/components/starter/footer/footer';
+import Header from "~/components/starter/header/header";
+import Footer from "~/components/starter/footer/footer";
 
-import styles from './styles.css?inline';
-import AttractionInformation from '~/components/attraction-information/attraction-information';
+import styles from "./styles.css?inline";
+import AttractionInformation from "~/components/attraction-information/attraction-information";
 
 export const useServerTimeLoader = routeLoader$(() => {
   return {
@@ -19,6 +19,7 @@ export default component$(() => {
     <>
       <Header />
       <main>
+        <Slot />
         <AttractionInformation />
       </main>
       <Footer />
