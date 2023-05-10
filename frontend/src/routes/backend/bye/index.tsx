603d2987247da0038208d5928af724a7be627a37
tispBe <timo.spring@zuehlke.com>
2023-05-10 16:12:10 +0200

    Remove obsolete BE endpoints

--- frontend/src/routes/backend/bye/index.tsx
+++
@@ -1,8 +0,0 @@
-import type { RequestHandler } from "@builder.io/qwik-city";
-
-export const onPost: RequestHandler = async (requestEvent) => {
-  const requestBody = await requestEvent.request.json();
-  console.log("request", requestBody);
-
-  requestEvent.json(200, { msg: "bye world" });
-};
--- frontend/src/routes/backend/hello/index.ts
+++
@@ -1,5 +0,0 @@
-import type { RequestHandler } from "@builder.io/qwik-city";
-
-export const onGet: RequestHandler = async (requestEvent) => {
-  requestEvent.json(200, { msg: "hello world" });
-};
