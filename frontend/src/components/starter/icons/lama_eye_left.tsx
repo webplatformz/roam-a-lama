8aa063e16ff113a6147d2cdbedbc114d97542be3
Schlumpf, Stefanie <stefanie.schlumpf2@zuehlke.com>
2023-05-10 14:57:17 +0200

    make llama's eyes following cursor

---
+++ frontend/src/components/starter/icons/lama_eye_left.tsx
@@ -0,0 +1,12 @@
+import styles from "~/components/starter/icons/lama.module.css";
+
+export const Lama_eye_left = () => (
+    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1"
+         xml:space="preserve" width="30" height="30" >
+        <g class="eye">
+            <circle cx="15" cy="15" r="15" />
+            <circle cx="17" cy="17" r="13" fill="var(--lama-white)" id='eyeball-left'/>
+            <circle cx="15" cy="15" r="8" fill="var(--lama-background)" id='pupil-left'/>
+        </g>
+    </svg>
+);
