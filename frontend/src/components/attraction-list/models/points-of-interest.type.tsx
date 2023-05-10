a36325a86cf420857f7b81880c372ccd95335ae3
tispBe <timo.spring@zuehlke.com>
2023-05-10 11:28:35 +0200

    Add List of points of interst

---
+++ frontend/src/components/attraction-list/models/points-of-interest.type.tsx
@@ -0,0 +1,40 @@
+import type { AttractionPhoto } from './attraction-photo';
+
+export type PointsOfInterest = {
+  business_status: string;
+  geometry: {
+    location: {
+      lat: number;
+      lng: number;
+    };
+    viewport: {
+      northeast: {
+        lat: number;
+        lng: number;
+      };
+      southwest: {
+        lat: number;
+        lng: number;
+      };
+    };
+  };
+  icon: string;
+  icon_background_color: string;
+  icon_mask_base_uri: string;
+  name: string;
+  opening_hours: {
+    open_now: boolean;
+  };
+  photos: AttractionPhoto[];
+  place_id: string;
+  plus_code: {
+    compound_code: string;
+    global_code: string;
+  };
+  rating: number;
+  reference: string;
+  scope: string;
+  types: string[];
+  user_ratings_total: number;
+  vicinity: string;
+};
