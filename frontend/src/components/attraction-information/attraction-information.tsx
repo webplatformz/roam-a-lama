d4902fd86ad0d37edcb9a71474e592f964f055f1
Schlumpf, Stefanie <stefanie.schlumpf2@zuehlke.com>
2023-05-12 11:26:33 +0200

    clean up

--- frontend/src/components/attraction-information/attraction-information.tsx
+++ frontend/src/components/attraction-information/attraction-information.tsx
@@ -3,7 +3,6 @@ import {
   component$,
   useResource$,
   useSignal,
-  useTask$,
   useVisibleTask$,
 } from '@builder.io/qwik';
 import styles from './attraction-information.module.css';
@@ -56,7 +55,9 @@ export default component$<AttractionInfo>((props) => {
         attractionFact.value = res?.choices[0].text;
       })
       .catch(() => {
-        attractionFact.value = 'Could not fetch information';
+        attractionFact.value = "Text-to-speech feature is now available on fairly any website, blog or newsletter like Substack. It's a game changer that you can listen to the content instead of reading it. Especially effective for people with visual or cognitive impairments or people who are on the go. I came up with the idea to implement it for my blog, so this is how I started doing a research on this topic which ended up being a tutorial for you. So in this tutorial, we will go through the process of building a text-to-speech component in React. We will use the Web Speech API to implement the text-to-speech functionality.";
+
+
       });
 
     return res;
