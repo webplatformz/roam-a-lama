8c390e2ec401d99910e86f9e4e80b6b2b06ab342
Schlumpf, Stefanie <stefanie.schlumpf2@zuehlke.com>
2023-05-12 11:26:25 +0200

    select voice and speed for text to speech

--- frontend/src/components/text-to-speech/index.tsx
+++ frontend/src/components/text-to-speech/index.tsx
@@ -1,5 +1,5 @@
 import type {Signal} from "@builder.io/qwik";
-import {component$, useSignal} from "@builder.io/qwik";
+import {component$, noSerialize, useSignal, useVisibleTask$} from "@builder.io/qwik";
 import styles from './text-to-speech.module.css';
 
 interface TextToSpeechProps {
@@ -9,33 +9,102 @@ interface TextToSpeechProps {
 export default component$<TextToSpeechProps>((props) => {
 
     const isPaused: Signal<boolean> = useSignal(false);
+    const isPlayed: Signal<boolean> = useSignal(false);
+    const voice = useSignal(null);
+    const rate = useSignal(1);
+
+    let voices: Signal<SpeechSynthesisVoice[]> = useSignal([])
+
+    useVisibleTask$(() => {
+        const interval = setInterval(() => {
+            voices.value = noSerialize(window.speechSynthesis.getVoices());
+            if (voice.value === null && voices.value.length > 0) {
+                voice.value = noSerialize(voices.value[0]);
+            }
+        }, 2000);
+        return () => {
+            clearInterval(interval);
+        };
+    });
 
     return (
         <>
-            <div class={styles.textToSpeech}>
-                <div>
-                    <button onClick$={() => {
-                        const synth = window.speechSynthesis;
-                        if (isPaused) {
-                            synth.resume();
+            {voices.value.length > 0 &&
+                <div class={styles.textToSpeech}>
+                    <div>
+                        {(!isPlayed.value) &&
+                            <button onClick$={() => {
+                                isPlayed.value = true;
+                                const synth = window.speechSynthesis;
+                                if (isPaused) {
+                                    synth.resume();
+                                }
+
+                                const utterance = new SpeechSynthesisUtterance(props.text);
+                                utterance.voice = voice.value;
+                                utterance.rate = rate.value;
+                                synth.speak(utterance);
+                            }}>{isPaused.value ? "Resume" : "Play"}</button>
                         }
-                        synth.speak(new SpeechSynthesisUtterance(props.text));
-                        isPaused.value = true;
-                    }}>{isPaused.value ? "Resume" : "Play"}</button>
-                    <button onClick$={() => {
-                        const synth = window.speechSynthesis;
-                        synth.pause();
-                        isPaused.value = true;
-                    }}>Pause
-                    </button>
-                    <button onClick$={() => {
-                        const synth = window.speechSynthesis;
-                        synth.cancel();
-                        isPaused.value = false;
-                    }}>Stop
-                    </button>
+                        {isPlayed.value &&
+                            <button onClick$={() => {
+                                const synth = window.speechSynthesis;
+                                synth.pause();
+                                isPlayed.value = false;
+                                isPaused.value = true;
+                            }}>Pause
+                            </button>
+                        }
+                        {(isPlayed.value || isPaused.value) &&
+                            <button onClick$={() => {
+                                const synth = window.speechSynthesis;
+                                synth.cancel();
+                                isPaused.value = false;
+                                isPlayed.value = false;
+                            }}>Stop
+                            </button>
+                        }
+                    </div>
+
+                    {(!isPlayed.value && !isPaused.value) &&
+                        <div class={styles.moreSettings}>
+
+
+                            <div>
+                                <label>
+                                    Voice:&nbsp;
+                                    <select value={voice.value.name} onChange$={(event) => {
+                                        // @ts-ignore
+                                        voice.value = noSerialize(voices.value.find((v: SpeechSynthesisVoice) => v.name === event.target.value));
+                                    }}>
+                                        {voices.value.map((v: SpeechSynthesisVoice) => (
+                                            <option key={v.name} value={v.name}>
+                                                {v.name}
+                                            </option>
+                                        ))}
+                                    </select>
+                                </label>
+                            </div>
+
+                            <div>
+                                <label>
+                                    Speed:&nbsp;
+                                    <input
+                                        type="range"
+                                        min="0.5"
+                                        max="2"
+                                        step="0.1"
+                                        value={rate.value}
+                                        onChange$={(event) => {
+                                            rate.value = parseFloat(event.target.value);
+                                        }}
+                                    />
+                                </label>
+                            </div>
+                        </div>
+                    }
                 </div>
-            </div>
+            }
 
         </>
     );
